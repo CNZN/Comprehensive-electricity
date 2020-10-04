86e5a4a5e29da44c642123838380cf4efb416c95
kenny <15737698693@163.com>
2020-10-04 19:57:41 +0800

    10.4

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,22 +1,51 @@
 <template>
-  <div>
+  <div class="datail">
     <el-page-header @back="goBack" content="详情页面"> </el-page-header>
+    <div class="content"></div>
+    <div class="footer">
+      <van-goods-action>
+        <van-goods-action-icon icon="chat-o" text="客服" @click="onClickIcon" />
+        <van-goods-action-icon
+          icon="cart-o"
+          text="购物车"
+          @click="onClickIcon"
+        />
+        <van-goods-action-icon icon="shop-o" text="店铺" @click="onClickIcon" />
+        <van-goods-action-button
+          type="danger"
+          text="立即购买"
+          @click="onClickButton"
+        />
+      </van-goods-action>
+    </div>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import { GoodsAction, GoodsActionIcon, GoodsActionButton } from "vant";
+import { Toast } from "vant";
+Vue.use(GoodsAction);
+Vue.use(GoodsActionButton);
+Vue.use(GoodsActionIcon);
 export default {
-    data(){
-        return{
-            id:null
-        }
-    },
+  data() {
+    return {
+      id: null,
+    };
+  },
   methods: {
     goBack() {
-      history.go(-1)
+      history.go(-1);
+    },
+    onClickIcon() {
+      Toast("点击图标");
+    },
+    onClickButton() {
+      Toast("点击按钮");
     },
   },
-  created(){
-      this.id = this.$route.params.id
-  }
+  created() {
+    this.id = this.$route.params.id;
+  },
 };
 </script>
--- src/view/car/Car.vue
+++ src/view/car/Car.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="video">
+  <div class="car">
+    <van-submit-bar :price="3050" button-text="提交订单" @submit="onSubmit">
+      <van-checkbox v-model="checked">全选</van-checkbox>
+      <!-- <template #tip>
+        你的收货地址不支持同城送,
+        <span @click="onClickEditAddress">修改地址</span>
+      </template> -->
+    </van-submit-bar>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import { SubmitBar } from "vant";
 
+Vue.use(SubmitBar);
 export default {
-  name: "Video",
-  components: {
-  },
+  name: "Car",
+  components: {},
 };
 </script>
 
